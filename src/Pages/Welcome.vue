229f7377acced8712874ee07d1e03c34dd29f7b9
Valeryia Matveyeva <matveyeva.valeryia@gmail.com>
2024-02-11 22:17:33 +0300

    Add Welcome page

--- src/Pages/Welcome.vue
+++ src/Pages/Welcome.vue
@@ -1,4 +1,18 @@
 <template>
-    <div>Welcome</div>
-    <router-link to="/lots">see lots</router-link>
+    <div class="flex h-screen">
+    <div class="w-5/12 my-gradient p-10 flex justify-end">
+        <div class="w-2/3">
+            <img class=" mb-4" src="../assets/logo-dark.svg" alt="Bidster">
+            <img class="w-full h-auto pr-8" src="../assets/element1.svg" alt="">
+        </div>
+    </div>
+    <div class="w-7/12 bg-my-black p-10 flex justify-start">
+        <div class="w-2/3">
+            <div class="text-my-gray4 text-5xl font-bold mb-4 mt-20">Supercharge your financial analytics with <span class="my-gradient-text">Bidster</span></div>
+            <div class="text-my-gray3 text-base font-light">Unlock the future of financial decision-making with our AI-powered analytics platform.</div>
+        </div>
+    </div>
+</div>
+
+
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,21 @@ export default {
         "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'my-black': '#130D1B',
+                'my-gray1': '#252134',
+                'my-gray2': '#262131',
+                'my-gray3': '#D8D8D9',
+                'my-gray4': '#FAFAFA',
+                'my-lila': '#AC6AFF',
+                'my-orange': '#FFC876',
+                'my-aqua': '#79FFF7',
+                'my-violet': '#9F53FF',
+                'my-pink': '#FF98E2',
+                'my-velvet': '#591952',
+            },
+        },
       },
     plugins: [],
 }
-
